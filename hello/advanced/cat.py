4959eb25b7e449d98f5beb404def0ffacde446f8
DESKTOP-6K2C8S3\RiverSafe <asad.khan@riversafe.co.uk>
2022-07-28 10:32:55 +0100

    Good and bad cat

--- hello/advanced/cat.py
+++ hello/advanced/cat.py
@@ -5,6 +5,17 @@ class cat:
         print("meow")
 
 
+
+class GoodCat(cat):
+    def meow(self):
+        print("purr purr")
+
+
+class BadCat(cat):
+    def meow(self):
+        print("hiss")
+
+
 c = cat()
 
 c.meow()
