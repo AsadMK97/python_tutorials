a95fa35991a9adae22dd7dec095f4dd3bcdccc2a
DESKTOP-6K2C8S3\RiverSafe <asad.khan@riversafe.co.uk>
2022-08-16 10:29:09 +0100

    data_tutorials added

---
+++ python/hello/basic/fib.py
@@ -0,0 +1,21 @@
+
+
+def fibonacci():
+    num1 = 0
+    num2 = 1
+    find = 2
+    fibnum=0
+    endnum=10
+    
+    while find <= endnum:
+        fibnum = (num1+num2)
+        num1 = num2
+        num2 = fibnum
+        find = (find+1)
+        continue
+    print (fibnum)
+        
+
+# user_input = input("Enter a number: ")
+# fibnum = fibonacci(user_input)
+fibonacci ()
