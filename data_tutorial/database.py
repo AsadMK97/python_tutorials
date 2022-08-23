43e7baefcb06de7e40686225455096f0b2c99d44
DESKTOP-6K2C8S3\RiverSafe <asad.khan@riversafe.co.uk>
2022-08-23 16:37:55 +0100

    black used on new files

--- data_tutorial/database.py
+++ data_tutorial/database.py
@@ -1,18 +1,20 @@
-#/usr/bin/python2.4
-#
-#
-
 import psycopg2
+import psycopg2.extras
 
-# Try to connect
 
 try:
-    conn = psycopg2.connect("dbname='template1' user='dbuser' password='mypass'")
+    conn = psycopg2.connect("dbname='weather' user='postgres' password='ferrari786'")
 except:
-    print ("I am unable to connect to the database.")
-    
+    print("I am unable to connect to the database.")
+
+
 cur = conn.cursor()
 try:
-    cur.execute("""DROP DATABASE foo_test""")
+    cur.execute("""SELECT * from weather""")
 except:
-    print ("I can't drop our test database!")
+    print("I can't SELECT from weather")
+
+rows = cur.fetchall()
+print("\nRows: \n")
+for row in rows:
+    print("   "), row[1]
